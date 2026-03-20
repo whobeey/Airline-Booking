8ba5d17f2976da1a9dc06a733c9c7547d88f86da
Wahab H <wahhab.eisa+GITHUB@gmail.com>
2026-03-20 10:17:46 +0000

    Started work on app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+"""
+This file is currently incomplete.
+"""
+
+# The main function of this module, it manages actions, data and it displays the main menu
+def run(): # The run() function does not take any argument or return any value when called
+    pass # "pass" statement is a temporary placeholder for future statements in the program
